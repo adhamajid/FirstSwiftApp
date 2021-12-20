628c800b4425dad6bd9238a0c5aa7ba3b0c372fa
adhamajid <adha1900018252@webmail.uad.ac.id>
2021-12-20 13:22:49 +0700

    Initial Commit

---
+++ PKM/PKMApp.swift
@@ -0,0 +1,17 @@
+//
+//  PKMApp.swift
+//  PKM
+//
+//  Created by Adha Syah Majid on 20/12/21.
+//
+
+import SwiftUI
+
+@main
+struct PKMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
