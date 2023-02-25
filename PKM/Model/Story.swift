db2cdf205b47307103d3fe9fe602e58fe2c1ab1a
adhamajid <adha1900018252@webmail.uad.ac.id>
2023-02-25 08:18:03 +0700

    fisrt_swiftApp

---
+++ PKM/Model/Story.swift
@@ -0,0 +1,20 @@
+//
+//  Story.swift
+//  PKM
+//
+//  Created by Adha Syah Majid on 20/12/21.
+//
+
+import SwiftUI
+
+struct Story: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Story_Previews: PreviewProvider {
+    static var previews: some View {
+        Story()
+    }
+}
---
+++ PKM/View/BlurView.swift
@@ -0,0 +1,20 @@
+//
+//  BlurView.swift
+//  PKM
+//
+//  Created by Adha Syah Majid on 20/12/21.
+//
+
+import SwiftUI
+
+struct BlurView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BlurView_Previews: PreviewProvider {
+    static var previews: some View {
+        BlurView()
+    }
+}
